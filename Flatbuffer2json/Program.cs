4d0051a7aa6a17541f718d402acc9205532f6669
shikwi <crpandas@163.com>
2025-10-26 00:02:10 +0800

    Update Flatbuffer2json

--- Flatbuffer2json/Program.cs
+++ Flatbuffer2json/Program.cs
@@ -16,60 +16,108 @@ namespace Flatbuffer2json
     {
         static void Main(string[] args)
         {
-#if false
-            //EncryptBytesFile dump
-            //Change filename to what you want to dump(lower case)
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: Flatbuffer2json.exe <bytes/db> <path to bytes folder or db file>");
+                return;
+            }
 
-            string inputBytesFile = "academyfavorscheduleexceltable.bytes";
-            string outputfile = Path.GetFileNameWithoutExtension(inputBytesFile) + ".json";
+            string mode = args[0].ToLower();
+            string path = args[1];
 
+            try
+            {
+                if (mode == "bytes")
+                {
+                    ProcessBytes(path);
+                }
+                else if (mode == "db")
+                {
+                    Processdb(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        static void ProcessBytes(string path)
+        {
+            //BytesFile dump
+            string outFolder = Path.Combine(path, "bytes2json");
+            if (!Directory.Exists(outFolder)) Directory.CreateDirectory(outFolder);
+
+            var files = Directory.GetFiles(path, "*.bytes");
             var keymap = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("BytesKey.json"));
-            var nameraw = keymap[inputBytesFile];
-            var bytes = File.ReadAllBytes(inputBytesFile);
-
             var converter = new EncryptTableConvert();
-            var jsonstring = converter.ConverToJson(nameraw, bytes);
 
-            File.WriteAllText(outputfile, jsonstring);
-            Console.WriteLine($"Successfully dump {inputBytesFile} to {outputfile}");
-#else
+            foreach (var inputBytesFile in files)
+            {
+                string outputfile = Path.Combine(outFolder, Path.GetFileNameWithoutExtension(inputBytesFile) + ".json");
+                var nameraw = keymap[Path.GetFileName(inputBytesFile)];
+                var bytes = File.ReadAllBytes(inputBytesFile);
+                var jsonstring = converter.ConverToJson(nameraw, bytes);
+                File.WriteAllText(outputfile, jsonstring);
+
+                Console.WriteLine($"Successfully dump {Path.GetFileName(inputBytesFile)}");
+            }
+        }
+
+        static void Processdb(string path)
+        {
             //DbBytes dump
-            //Change dbtablename to tablename from sqlite db
+            string outFolder = Path.Combine(Path.GetDirectoryName(path), "db2json");
+            if (!Directory.Exists(outFolder)) Directory.CreateDirectory(outFolder);
 
-            string dbPath = "./ExcelDB.db";
-            string dbtablename = "CharacterVoiceDBSchema";
-
-            var ouputfile = $"{dbtablename}.json";
             var converter = new DbTableConvert();
-            using (var connection = new SQLiteConnection($"Data Source={dbPath}"))
+            using (var connection = new SQLiteConnection($"Data Source={path}"))
             {
                 connection.Open();
 
-                string query = $"SELECT * FROM {dbtablename}";
-                using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                var tableNames = new List<string>();
+                string tablequery = "SELECT name FROM sqlite_master WHERE type='table'";
+                using (SQLiteCommand cmd = new SQLiteCommand(tablequery, connection))
                 {
-                    var alldata = new List<dynamic>();
                     using (SQLiteDataReader reader = cmd.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            string tableName = dbtablename.Replace("DBSchema", "Excel");
-                            byte[] bytes = (byte[])reader["Bytes"];
-
-                            string jsonstring = converter.ConverToJson(tableName, bytes);
-                            var rowstring = JsonConvert.DeserializeObject<dynamic>(jsonstring);
-                            alldata.Add(rowstring);
+                            string tableName = reader.GetString(0);
+                            tableNames.Add(tableName);
                         }
+                    }
+                }
 
-                        string jsonout = JsonConvert.SerializeObject(alldata, Formatting.Indented);
-                        File.WriteAllText(ouputfile, jsonout);
-                        Console.WriteLine($"Successfully dump {dbtablename} to {ouputfile}");
+                foreach (var dbtablename in tableNames)
+                {
+                    string tableName = dbtablename.Replace("DBSchema", "Excel");
+                    string outputfile = Path.Combine(outFolder, tableName + ".json");
+
+                    var alldata = new List<dynamic>();
+                    string query = $"SELECT * FROM {dbtablename}";
+
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                    {
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                byte[] bytes = (byte[])reader["Bytes"];
+
+                                string jsonstring = converter.ConverToJson(tableName, bytes);
+                                var rowstring = JsonConvert.DeserializeObject<dynamic>(jsonstring);
+                                alldata.Add(rowstring);
+                            }
+
+                            string jsonout = JsonConvert.SerializeObject(alldata, Formatting.Indented);
+                            File.WriteAllText(outputfile, jsonout);
+                            Console.WriteLine($"Successfully dump {dbtablename}");
+                        }
                     }
                 }
 
             }
-
-#endif
         }
 
     }
